980db4bc19c18fc3bbcf67cf02601f1b0c7744da
Agustín More <amore@fi.uba.ar>
2018-11-14 22:03:33 -0300

    Added fechautil, modify analog header

--- analog.c
+++ analog.c
@@ -2,19 +2,43 @@
 #include "fechautil.h"
 #include "hash.h"
 #include "strutil.h"
+#include <string.h>
 typedef struct vuelo {
   int numero;
   char *aerolinea;
-  char *fecha_t *fecha;
-
+  char *origen;
+  char *destino;
+  char *tail_number;
+  int prioridad;
+  fecha_t *fecha;
+  int departure_delay;
+  int tiempo_vuelo;
+  int cancelado;
 } vuelo_t;
+
+int vueloscmp(void *a, void *b) {
+  vuelo_t *v_a = (vuelo_t *)a;
+  vuelo_t *v_b = (vuelo_t *)b;
+  return v_a->numero - v_b->numero;
+}
+
+// ESTA SIRVE PARA EL HEAP DE PRIORIDAD
+// int vueloscmp(void *a, void *b) {
+//   vuelo_t *v_a = (vuelo_t *)a;
+//   vuelo_t *v_b = (vuelo_t *)b;
+//   int fecha_comp = fechacmp(v_a->fecha, v_b->fecha);
+//   if (fecha_comp != 0)
+//     return fecha_comp;
+//   return v_a->numero - v_b->numero;
+// }
+
 hash_t *iniciar_vuelos() {
   hash_t *vuelos = hash_crear(vueloscmp);
   if (!vuelos)
     return NULL;
-
   return vuelos;
 }
+
 bool agregar_archivo(hash_t *vuelos, const char *nombre_archivo) {}
 bool ver_tablero(hash_t *vuelos, int cant_vuelos, const char *modo,
                  fecha_t desde, fecha_t hasta) {}
