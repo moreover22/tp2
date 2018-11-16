f98bdf28d0c24e9670511cdfef2a9686cd5ed5d5
franckestein11 <francomariotti9@gmail.com>
2018-11-15 22:20:11 -0300

    funcion agregar_archivo

--- analog.c
+++ analog.c
@@ -32,10 +32,56 @@ hash_t *iniciar_vuelos() {
     return NULL;
   return vuelos;
 }
+void inicializar_vuelo(char**datos,vuelo_t*vuelo){
+	vuelo->numero=atoi(datos[0]);
+	vuelo->aerolinea=datos[1]);
+	vuelo->origen=datos[2];
+	vuelo->destino=datos[3];
+	vuelo->tail_number=datos[4];
+	vuelo->prioridad=atoi(datos[5]);
+	vuelo->fecha=malloc(sizeof(fecha_t));
+	vuelo->departure_delay=atoi(datos[7]);
+	vuelo->tiempo_vuelo=atoi(datos[8]);
+	vuelo->cancelado=atoi(datos[9]);
+}
 
-bool agregar_archivo(hash_t *vuelos, const char *nombre_archivo) {}
+bool agregar_archivo(hash_t* vuelos,abb_t*  tablero_vuelos,const char *nombre_archivo) {
+	
+	FILE* archivo_vuelos=fopen(nombre_archivo,"r");
+	
+	if(!archivo_vuelos)return false;
+	
+	char* linea=NULL;
+	size_t cantidad=0;
+	
+	ssize_t leidos=0;
+	size_t contador=0;
+	
+	while((leidos=getline(linea,&cantidad,archivo_vuelos)>0){
+		linea[leidos-1]='\0';
+		char** datos_vuelo=split(linea,",");
+		char *clave_vuelo=datos[0];
+		vuelo_t* vuelo=NULL;
+		if(hash_pertenece(hash,clave)){
+			vuelo=(vuelo_t*)hash_obtener(hash,clave);
+			inicializar_vuelo(datos,vuelo);
+		}
+		else{
+			vuelo_t*vuelo=malloc(sizeof(vuelo_t));
+			inicializar_vuelo(datos,vuelo);
+			hash_guardar(hash,clave_vuelo,vuelo);
+			
+			abb_guardar(tablero_vuelos,clave,NULL);
+		}
+		free(datos);
+	}
+	
+	return true;
+}
+
 bool ver_tablero(hash_t *vuelos, int cant_vuelos, const char *modo,
                  fecha_t desde, fecha_t hasta) {}
+                 
 bool info_vuelo(hash_t *vuelos, const char *cod_vuelo) {
   vuelo_t *vuelo = (vuelo_t *)hash_obtener(vuelos, cod_vuelo);
   if (!vuelo)
